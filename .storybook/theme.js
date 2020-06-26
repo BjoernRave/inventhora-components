4ace69a1b83475cedaea40a973324e8ccfdf966a
BjoernRave <bjoern.rave@gmail.com>
2020-06-26 23:56:59 +0100

    theme storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'light',
+  colorPrimary: '#3f51b5',
+  colorSecondary: '#3c9f80',
+  brandImage: 'https://inventhora.com/logo_large.png',
+  brandUrl: 'https://inventhora.com',
+  brandTitle: 'Inventhora - Inventory Management System',
+})
